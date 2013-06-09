6ccbce00e8d03e8f2ea8c89963a097012859a76c
duoduo <zerolin_0428@163.com>
2013-06-09 13:02:36 +0800

    new resp PARK_SYS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkSys::Application.config.secret_token = 'fee26862e24af1e702b3c375d3127798f13606ed133a8abda1b3a22bb01b0a6e80f8c840e26da9842fed5abbbbe43d91f8e0b438f35df67d197cf54f93d9d544'
