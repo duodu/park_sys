2099acfe637a3bdfdfee9bc3fcb883dfe1fd745e
duoduo <zerolin_0428@163.com>
2013-06-09 15:28:08 +0800

    model stall

--- app/models/stall.rb
+++ app/models/stall.rb
@@ -1,5 +1,5 @@
 class Stall < ActiveRecord::Base
-  attr_accessible :is_idle, :location, :park_his_id #is_idle=0:不空闲；is_idle=1:空闲
+  attr_accessible :is_idle, :location #is_idle=0:不空闲；is_idle=1:空闲
   has_many :park_histories, :class_name => "ParkHistory", :dependent => :nullify, :conditions => "is_out=0"
   validates :is_idle, :location, :presence => true
   validates :is_idle, :numericality => true
